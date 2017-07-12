fa8f64fb7850c38336907c1ed176f392026ce02d
ovarunendra <vpsingh016@gmail.com>
2017-07-12 22:48:54 +0530

    Added styling to welcome screen

--- components/Slides.js
+++ components/Slides.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 class Slides extends Component {
+    renderSlides() {
+        return this.props.data.map((slide) => {
+            return (
+                <View key={slide.text} 
+                    style={[styles.slideStyle, { backgroundColor: slide.color }]}>
+                    <Text style={styles.textStyle}>{slide.text}</Text>
+                </View>
+            );
+        })
+    }
+
     render() {
         return (
-            <View>
-
+            <View style={{ height: SCREEN_HEIGHT }}>
+                <ScrollView
+                    horizontal
+                    style={{ flex: 1 }}
+                    pagingEnabled
+                >
+                    {this.renderSlides()}
+                </ScrollView>
             </View>
         );
+    }
+}
+
+const styles = {
+    slideStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: SCREEN_WIDTH
+    },
+    textStyle: {
+        fontSize: 30,
+        color: 'white'
     }
 }
 
