4e07e8eecbde37a7b11430f522f197b56b745bd3
ovarunendra <vpsingh016@gmail.com>
2017-07-12 21:58:48 +0530

    nested navigator

--- App.js
+++ App.js
@@ -4,12 +4,20 @@ import { TabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
 export default class App extends React.Component {
   render() {
     let MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen }
+        })
+      }
     });
 
     return (
