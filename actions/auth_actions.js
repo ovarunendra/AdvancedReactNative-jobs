9b106a6c2ae6ff3e3b6ace86b805483593c3567d
ovarunendra <vpsingh016@gmail.com>
2017-07-13 08:57:26 +0530

    Added async await

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -4,6 +4,12 @@ import {
     FACEBOOK_LOGIN_SUCCESS
 } from './types';
 
-export const facebookLogin = () => {
+export const facebookLogin = async dispacth => {
+    let token = await AsyncStorage.getItem('fb_token');
 
+    if (token) {
+        // Dispacth an action saying FB login is done
+    } else {
+        // Start up FB Login Process
+    }
 };
