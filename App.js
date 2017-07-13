e3a5c04012adb44b9cb67abdb266364b1402a66c
ovarunendra <vpsingh016@gmail.com>
2017-07-13 20:41:09 +0530

    register callback

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
+import { Notifications } from 'expo';
 
+import registerForNotifications from './services/push_notifications';
 import store from './store';
 
 import AuthScreen from './screens/AuthScreen';
@@ -13,6 +15,20 @@ import SettingsScreen from './screens/SettingsScreen';
 import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+
+      if (origin === 'received' && text) {
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'Ok.' }]
+        )
+      }
+    })
+  }
   render() {
     let MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
